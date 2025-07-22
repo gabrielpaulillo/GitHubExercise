ca3112d4cee0874a2c7cc99cce4fa32f46a8b723
Gabriel Paulillo <gabrielpaulillo@gmail.com>
2025-07-22 12:03:05 -0300

    Update Teste.cs
    Inserido comando de print com o nome "Gabriel"

--- Assets/Scripts/Teste.cs
+++ Assets/Scripts/Teste.cs
@@ -9,5 +9,6 @@ public class Teste : MonoBehaviour
         //Para completar o desafio, inclua um novo comando print com o seu nome
         print("Igor");
         print("Raquel");
+        print("Gabriel");
     }
 }
